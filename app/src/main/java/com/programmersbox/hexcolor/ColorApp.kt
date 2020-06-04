00f7f360cfc3a89bc884d5d7304cbb3113de0d51
jakepurple13 <jakerein@gmail.com>
2020-06-04 14:04:14 -0400

    -translation setup just cause

--- app/src/main/java/com/programmersbox/hexcolor/ColorApp.kt
+++ app/src/main/java/com/programmersbox/hexcolor/ColorApp.kt
@@ -1,6 +1,7 @@
 package com.programmersbox.hexcolor
 
 import android.app.Application
+import android.os.Build
 import com.programmersbox.helpfulutils.NotificationChannelImportance
 import com.programmersbox.helpfulutils.createNotificationChannel
 import com.programmersbox.loggingutils.Loged
@@ -10,7 +11,9 @@ class ColorApp : Application() {
         super.onCreate()
         Loged.FILTER_BY_PACKAGE_NAME = "programmersbox"
 
-        createNotificationChannel("hexcolor", importance = NotificationChannelImportance.HIGH)
+        createNotificationChannel("hexcolorer", importance = NotificationChannelImportance.HIGH) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) setAllowBubbles(true)
+        }
 
     }
 }
